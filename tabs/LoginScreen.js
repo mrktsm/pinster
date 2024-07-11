0e62e7cebb030bcd8316ef2351788295e086c344
Marko Tsymbaluk <markotsymbaluk@dhcp-76-204.cc.gettysburg.edu>
2024-07-11 02:03:39 -0400

    Last decent version

---
+++ tabs/LoginScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default class LoadingScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text> Login Screen </Text>
+            </View>
+        )
+    }
+}
+
